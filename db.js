3bd7f27134457524b341b8368d8a14f558ce011a
Wilson Montero <wilsommonteroen14@gmail.com>
2025-10-16 22:48:18 -0400

    Configurar Sequelize para despliegue en Render

--- db.js
+++ db.js
@@ -1,14 +1,30 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-  }
-);
+// Render nos dará esta variable de entorno automáticamente
+const connectionUrl = process.env.DATABASE_URL;
+
+const sequelize = connectionUrl
+  // Si estamos en producción (Render), usa la URL con SSL
+  ? new Sequelize(connectionUrl, {
+      dialect: 'postgres',
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false // Requerido para conexiones de Render
+        }
+      },
+      logging: false,
+    })
+  // Si estamos en local, usa las variables de .env de siempre
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASS,
+      {
+        host: process.env.DB_HOST,
+        dialect: 'postgres'
+      }
+    );
 
 module.exports = sequelize;
